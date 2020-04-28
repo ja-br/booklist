b63befed53a1ba1091fbcc9c66702eba044c539e
ja-br <letstalk@jamesbrooks.tech>
2020-04-28 11:10:44 -0400

    Submit adds entry to table UI

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ function Book(title, author, isbn){
 
 function UI(){}
 
+UI.prototype.addBookToTable = function(book){
+    const list = document.querySelector('#book-list')
+    const row = document.createElement('tr')
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">x</a></td>
+    `
+
+    list.appendChild(row)
+}
+
 document.querySelector('#book-form').addEventListener('submit',(e)=>{
     const title = document.querySelector('#title').value,
         author = document.querySelector('#author').value,
@@ -13,5 +27,10 @@ document.querySelector('#book-form').addEventListener('submit',(e)=>{
         
 
     const book = new Book(title, author, isbn)
+
+    const ui = new UI()
+
+    ui.addBookToTable(book)
     e.preventDefault()
 })
+
