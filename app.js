122b84196c88a2026c1c7f1efbc740fd815f937a
ja-br <letstalk@jamesbrooks.tech>
2020-04-28 10:57:16 -0400

    Submit instantiates new book object

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ function Book(title, author, isbn){
 function UI(){}
 
 document.querySelector('#book-form').addEventListener('submit',(e)=>{
-    console.log('test')
+    const title = document.querySelector('#title').value,
+        author = document.querySelector('#author').value,
+        isbn = document.querySelector('#isbn').value
+        
+
+    const book = new Book(title, author, isbn)
     e.preventDefault()
 })
