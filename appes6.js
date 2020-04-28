e18ae149ea9f5ad1001efc6ad0c11919dd01befa
ja-br <letstalk@jamesbrooks.tech>
2020-04-28 11:55:33 -0400

    Refactored to use es6 Class syntax

---
+++ appes6.js
@@ -0,0 +1,93 @@
+class Book {
+    constructor(title, author, isbn){
+        this.title = title
+        this.author = author
+        this.isbn = isbn
+    }
+}
+
+class UI {
+    addBookToTable(book){
+        const list = document.querySelector('#book-list')
+        const row = document.createElement('tr')
+    
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">x</a></td>
+        `
+    
+        list.appendChild(row)
+    }
+
+    showAlert(msg, className){
+        const div = document.createElement('div')
+        const container = document.querySelector('.container')
+        const form = document.querySelector('#book-form')
+    
+        div.classList.add('alert',`${className}`)
+    
+        div.appendChild(document.createTextNode(msg))
+    
+        container.insertBefore(div, form)
+    
+        setTimeout(()=>{
+            document.querySelector('.alert')
+                .remove()
+        }, 2000)
+    }
+
+    deleteBook(target){
+        UI.prototype.deleteBook = function(target){
+            if(target.className === 'delete'){
+                target.parentElement.parentElement.remove()
+            }
+        }
+    }
+
+    clearFields(){
+        document.querySelector('#title').value = ''
+        document.querySelector('#author').value = ''
+        document.querySelector('#isbn').value = ''
+    }
+
+}
+
+// Listener for submission
+document.querySelector('#book-form').addEventListener('submit',(e)=>{
+    const title = document.querySelector('#title').value,
+        author = document.querySelector('#author').value,
+        isbn = document.querySelector('#isbn').value
+        
+
+    const book = new Book(title, author, isbn)
+
+    const ui = new UI()
+
+    // Validation
+    if(title === '' || author === '' || isbn === ''){
+        ui.showAlert('Please fill in all fields', 'error')
+    } else {
+        ui.showAlert('Book Added!', 'success')
+        ui.addBookToTable(book)
+        ui.clearFields()
+    }
+
+ 
+
+    e.preventDefault()
+})
+
+
+// Listener for delete
+
+document.querySelector('#book-list').addEventListener('click', (e)=>{
+    const ui = new UI()
+
+    ui.deleteBook(e.target)
+
+    ui.showAlert('Book deleted', 'success')
+
+    e.preventDefault()
+})
